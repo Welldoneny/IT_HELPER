d970483dafae53ad334557ebb7a76b0c228e858c
Welldoneny <VladislavIzov@gmail.com>
2026-05-04 18:13:51 +0700

    fix morse

--- lib/liba/morse.dart
+++ lib/liba/morse.dart
@@ -84,6 +84,8 @@ class Morse {
     for (int i = 0; i < plaintext.length; i++) {
       if (_morseCode.containsKey(plaintext[i])) {
         morsetext += _morseCode[plaintext[i]]!;
+      } else {
+        continue;
       }
       morsetext += ' ';
     }
